b9819db6af6af20af3b8efacf88d20f3fee43054
王大松 <dasong.wds@alibaba-inc.com>
2019-08-31 18:00:34 +0800

    git ignore

---
+++ src/com/leetcode/offer/day25/Intersection.java
@@ -0,0 +1,53 @@
+package com.leetcode.offer.day25;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author duson
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
+ * 说明:
+ * <p>
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Intersection {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) {
+            return nums1;
+        }
+        if (nums2 == null || nums2.length == 0) {
+            return nums2;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums1) {
+            set.add(num);
+        }
+        Set<Integer> result = new HashSet<>();
+        for (Integer num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+        int[] array = new int[result.size()];
+        int k = 0;
+        for (int t : result) {
+            array[k++] = t;
+        }
+        return array;
+    }
+}
