a0aa8c46429f748b33185e3c72d48003cdb6247a
DusongWang <dasong.wds@alibaba-inc.com>
2019-09-06 14:56:00 +0800

    构建树增加注释

--- src/com/leetcode/offer/day12/BuildTree2.java
+++ src/com/leetcode/offer/day12/BuildTree2.java
@@ -3,23 +3,23 @@ package com.leetcode.offer.day12;
 /**
  * @author duson
  * 根据一棵树的中序遍历与后序遍历构造二叉树。
- *
+ * <p>
  * 注意:
  * 你可以假设树中没有重复的元素。
- *
+ * <p>
  * 例如，给出
- *
+ * <p>
  * 中序遍历 inorder = [9,3,15,20,7]
  * 后序遍历 postorder = [9,15,7,20,3]
  * 返回如下的二叉树：
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- *
- *
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -56,6 +56,11 @@ public class BuildTree2 {
             }
         }
         TreeNode root = new TreeNode(val);
+
+        /*
+         * left numbers  = k-1-inl
+         * right numbers = inr-k-1;
+         * */
         root.left = build(inorder, postorder, inl, k - 1, postl, k - 1 - inl + postl);
         root.right = build(inorder, postorder, k + 1, inr, postr + k - inr, postr - 1);
         return root;
