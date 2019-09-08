f5b691852172e322c54dface958e26db530b446e
DusongWang <dasong.wds@alibaba-inc.com>
2019-09-08 22:13:25 +0800

    0908

---
+++ src/com/leetcode/offer/day29/ContainsNearbyDuplicate.java
@@ -0,0 +1,54 @@
+package com.leetcode.offer.day29;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duson
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ * 示例 3:
+ * <p>
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ContainsNearbyDuplicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsNearbyDuplicate containsNearbyDuplicate = new ContainsNearbyDuplicate();
+        System.out.println(containsNearbyDuplicate.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+        System.out.println(containsNearbyDuplicate.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+        System.out.println(containsNearbyDuplicate.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+
+    }
+}
