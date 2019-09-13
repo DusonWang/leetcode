ece2ef5db12ffd13c0b57fe421fc46904a01072d
DusongWang <dasong.wds@alibaba-inc.com>
2019-09-13 19:43:52 +0800

    0913

---
+++ src/com/leetcode/offer/day29/IsPerfectSquare.java
@@ -0,0 +1,32 @@
+package com.leetcode.offer.day29;
+
+/**
+ * @author duson
+ * 给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+ * <p>
+ * 说明：不要使用任何内置的库函数，如  sqrt。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：16
+ * 输出：True
+ * 示例 2：
+ * <p>
+ * 输入：14
+ * 输出：False
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-perfect-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsPerfectSquare {
+
+    public boolean isPerfectSquare(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+}
