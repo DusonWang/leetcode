8ee867f5af80bea40c961b168fd19617193cd17e
DusongWang <dasong.wds@alibaba-inc.com>
2019-09-16 22:51:43 +0800

    create-maximum-number

---
+++ src/com/leetcode/offer/day30/CreateMaximumNumber.java
@@ -0,0 +1,83 @@
+package com.leetcode.offer.day30;
+
+/**
+ * @author duson
+ * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
+ * <p>
+ * 求满足该条件的最大数。结果返回一个表示该最大数的长度为 k 的数组。
+ * <p>
+ * 说明: 请尽可能地优化你算法的时间和空间复杂度。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * nums1 = [3, 4, 6, 5]
+ * nums2 = [9, 1, 2, 5, 8, 3]
+ * k = 5
+ * 输出:
+ * [9, 8, 6, 5, 3]
+ * 示例 2:
+ * <p>
+ * 输入:
+ * nums1 = [6, 7]
+ * nums2 = [6, 0, 4]
+ * k = 5
+ * 输出:
+ * [6, 7, 6, 0, 4]
+ * 示例 3:
+ * <p>
+ * 输入:
+ * nums1 = [3, 9]
+ * nums2 = [8, 9]
+ * k = 3
+ * 输出:
+ * [9, 8, 9]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/create-maximum-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CreateMaximumNumber {
+
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        int[] res = new int[k];
+        for (int i = Math.max(0, k - n); i <= k && i <= m; i++) {
+            int[] arr = merge(maxArr(nums1, i), maxArr(nums2, k - i), k);
+            if (gt(arr, 0, res, 0)) {
+                res = arr;
+            }
+        }
+        return res;
+    }
+
+    private int[] maxArr(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[k];
+        for (int i = 0, j = 0; i < n; i++) {
+            while (n - i + j > k && j > 0 && nums[i] > res[j - 1]) {
+                j--;
+            }
+            if (j < k) {
+                res[j++] = nums[i];
+            }
+        }
+        return res;
+    }
+
+    private int[] merge(int[] nums1, int[] nums2, int k) {
+        int[] res = new int[k];
+        for (int i = 0, j = 0, r = 0; r < k; r++) {
+            res[r] = gt(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
+        }
+        return res;
+    }
+
+    private boolean gt(int[] nums1, int i, int[] nums2, int j) {
+        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+            i++;
+            j++;
+        }
+        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+    }
+}
