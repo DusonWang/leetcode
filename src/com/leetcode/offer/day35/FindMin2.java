8b565a8d4594cfa9767f0580946b315159c618b1
DusongWang <dasong.wds@alibaba-inc.com>
2019-09-27 15:24:25 +0800

    find-minimum-in-rotated-sorted-array-ii

--- src/com/leetcode/offer/day35/FindMin2.java
+++ src/com/leetcode/offer/day35/FindMin2.java
@@ -8,22 +8,26 @@ package com.leetcode.offer.day35;
  * <p>
  * 请找出其中最小的元素。
  * <p>
- * 你可以假设数组中不存在重复元素。
+ * 注意数组中可能存在重复的元素。
  * <p>
- * 示例 1:
+ * 示例 1：
  * <p>
- * 输入: [3,4,5,1,2]
+ * 输入: [1,3,5]
  * 输出: 1
- * 示例 2:
+ * 示例 2：
  * <p>
- * 输入: [4,5,6,7,0,1,2]
+ * 输入: [2,2,2,0,1]
  * 输出: 0
+ * 说明：
+ * <p>
+ * 这道题是 寻找旋转排序数组中的最小值 的延伸题目。
+ * 允许重复会影响算法的时间复杂度吗？会如何影响，为什么？
  * <p>
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+ * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class FindMin {
+public class FindMin2 {
 
     public int findMin(int[] nums) {
         int l = 0;
@@ -32,10 +36,17 @@ public class FindMin {
             int m = l + (h - l) / 2;
             if (nums[m] > nums[h]) {
                 l = m + 1;
-            } else {
+            } else if (nums[m] < nums[h]) {
                 h = m;
+            } else {
+                h--;
             }
         }
         return nums[l];
+    }
+
+    public static void main(String[] args) {
+        FindMin2 findMin2 = new FindMin2();
+        System.out.println(findMin2.findMin(new int[]{3, 3, 1, 3}));
     }
 }
