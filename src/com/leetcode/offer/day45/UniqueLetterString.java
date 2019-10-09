5b3b4107f676ba9ba8acaed1839c508a9ff7d9b9
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-09 16:06:34 +0800

    unique-letter-string

--- src/com/leetcode/offer/day45/UniqueLetterString.java
+++ src/com/leetcode/offer/day45/UniqueLetterString.java
@@ -35,7 +35,7 @@ package com.leetcode.offer.day45;
  * 说明: 0 <= S.length <= 10000。
  * <p>
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/c
+ * 链接：https://leetcode-cn.com/problems/unique-letter-string/
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class UniqueLetterString {
