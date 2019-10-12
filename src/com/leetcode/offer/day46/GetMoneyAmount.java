d023bee5a104cbb44791226779c67c60a80ae239
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-12 11:45:55 +0800

    guess-number-higher-or-lower-ii 2

--- src/com/leetcode/offer/day46/GetMoneyAmount.java
+++ src/com/leetcode/offer/day46/GetMoneyAmount.java
@@ -58,4 +58,19 @@ public class GetMoneyAmount {
         }
         return dp[start][end] = min;
     }
+
+    public int getMoneyAmount2(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int len = 2; len <= n; len++) {
+            for (int start = 1; start <= n - len + 1; start++) {
+                int minres = Integer.MAX_VALUE;
+                for (int piv = start; piv < start + len - 1; piv++) {
+                    int res = piv + Math.max(dp[start][piv - 1], dp[piv + 1][start + len - 1]);
+                    minres = Math.min(res, minres);
+                }
+                dp[start][start + len - 1] = minres;
+            }
+        }
+        return dp[1][n];
+    }
 }
