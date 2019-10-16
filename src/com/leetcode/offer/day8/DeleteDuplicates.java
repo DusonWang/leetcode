6a7e79321600877937ccd77e0f8d7d7a802a8c6b
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-17 06:04:23 +0800

    opt

--- src/com/leetcode/offer/day8/DeleteDuplicates.java
+++ src/com/leetcode/offer/day8/DeleteDuplicates.java
@@ -28,6 +28,23 @@ public class DeleteDuplicates {
         }
     }
 
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode list = head;
+
+        while(list != null) {
+            if (list.next == null) {
+                break;
+            }
+            if (list.val == list.next.val) {
+                list.next = list.next.next;
+            } else {
+                list = list.next;
+            }
+        }
+
+        return head;
+    }
+
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
         while (cur != null && cur.next != null) {
