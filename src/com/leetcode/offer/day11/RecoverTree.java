f5e9b6ac92c25a89baf30071d214a83e47f3e416
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-17 12:01:43 +0800

    opt

--- src/com/leetcode/offer/day11/RecoverTree.java
+++ src/com/leetcode/offer/day11/RecoverTree.java
@@ -1,54 +1,52 @@
 package com.leetcode.offer.day11;
 
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Stack;
 
 /**
  * @author duson
  * 二叉搜索树中的两个节点被错误地交换。
- *
+ * <p>
  * 请在不改变其结构的情况下，恢复这棵树。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: [1,3,null,null,2]
- *
+ * <p>
  *    1
  *   /
  *  3
  *   \
  *    2
- *
+ * <p>
  * 输出: [3,1,null,null,2]
- *
+ * <p>
  *    3
  *   /
  *  1
  *   \
  *    2
  * 示例 2:
- *
+ * <p>
  * 输入: [3,1,4,null,null,2]
- *
- *   3
- *  / \
+ * <p>
+ * 3
+ * / \
  * 1   4
  *    /
  *   2
- *
+ * <p>
  * 输出: [2,1,4,null,null,3]
- *
- *   2
- *  / \
+ * <p>
+ * 2
+ * / \
  * 1   4
  *    /
- *   3
+ *  3
  * 进阶:
- *
+ * <p>
  * 使用 O(n) 空间复杂度的解法很容易实现。
  * 你能想出一个只使用常数空间的解决方案吗？
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/recover-binary-search-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -63,6 +61,38 @@ public class RecoverTree {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    private TreeNode firstElement = null;
+    private TreeNode secondElement = null;
+    private TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree3(TreeNode root) {
+
+        traverse(root);
+        int temp = firstElement.val;
+        firstElement.val = secondElement.val;
+        secondElement.val = temp;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.left);
+
+        if (firstElement == null && prevElement.val >= root.val) {
+            firstElement = prevElement;
+        }
+
+        if (firstElement != null && prevElement.val >= root.val) {
+            secondElement = root;
+        }
+
+        prevElement = root;
+
+        traverse(root.right);
     }
 
     public void recoverTree(TreeNode root) {
