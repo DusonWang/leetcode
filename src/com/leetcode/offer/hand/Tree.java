43a8932eaacdd693f6480fdf316b9ebc2641463e
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-17 09:31:14 +0800

    tree visit

--- src/com/leetcode/offer/hand/Tree.java
+++ src/com/leetcode/offer/hand/Tree.java
@@ -1,5 +1,10 @@
 package com.leetcode.offer.hand;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author duson
  */
@@ -15,4 +20,99 @@ public class Tree {
         }
     }
 
+    private List<Integer> preOrder(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        backPreOrder(root, list);
+        return list;
+    }
+
+    private void backPreOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        backPreOrder(root.left, list);
+        backPreOrder(root.right, list);
+    }
+
+    private List<Integer> preOrder2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        stack.push(root);
+        while (stack.size() != 0) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+    private List<Integer> inOrder(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            list.add(curr.val);
+            curr = curr.right;
+        }
+        return list;
+    }
+
+    private List<Integer> postOrder(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+        stack.push(root);
+        while (stack.size() != 0) {
+            TreeNode node = stack.pop();
+            list.addFirst(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return list;
+    }
+
+    private List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        backLevel(root, 0, list);
+        return list;
+    }
+
+    private void backLevel(TreeNode node, int level, List<List<Integer>> list) {
+        if (list.size() == level) {
+            list.add(new ArrayList<>());
+        }
+        list.get(level).add(node.val);
+        if (node.left != null) {
+            backLevel(node.left, level + 1, list);
+        }
+        if (node.right != null) {
+            backLevel(node.right, level + 1, list);
+        }
+    }
 }
