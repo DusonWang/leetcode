9b3926cdc22a1b4e21011d4009009fba8e286986
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-28 14:46:14 +0800

    rotate-string

---
+++ src/com/leetcode/offer/day48/RotateString.java
@@ -0,0 +1,32 @@
+package com.leetcode.offer.day48;
+
+/**
+ * @author duson
+ * 给定两个字符串, A 和 B。
+ * <p>
+ * A 的旋转操作就是将 A 最左边的字符移动到最右边。 例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea' 。如果在若干次旋转操作之后，A 能变成B，那么返回True。
+ * <p>
+ * 示例 1:
+ * 输入: A = 'abcde', B = 'cdeab'
+ * 输出: true
+ * <p>
+ * 示例 2:
+ * 输入: A = 'abcde', B = 'abced'
+ * 输出: false
+ * 注意：
+ * <p>
+ * A 和 B 长度不超过 100。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RotateString {
+
+    public boolean rotateString(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        return (a + a).contains(b);
+    }
+}
