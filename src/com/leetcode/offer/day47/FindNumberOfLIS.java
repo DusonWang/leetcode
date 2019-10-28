fac3371270f45b547467f4cb90ad1e048bd8e079
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-28 13:37:36 +0800

    number-of-longest-increasing-subsequence

---
+++ src/com/leetcode/offer/day47/FindNumberOfLIS.java
@@ -0,0 +1,49 @@
+package com.leetcode.offer.day47;
+
+/**
+ * @author duson
+ * 给定一个未排序的整数数组，找到最长递增子序列的个数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,5,4,7]
+ * 输出: 2
+ * 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+ * 示例 2:
+ * <p>
+ * 输入: [2,2,2,2,2]
+ * 输出: 5
+ * 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+ * 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindNumberOfLIS {
+
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length, res = 0, maxLen = 0;
+        int[] len = new int[n], cnt = new int[n];
+        for (int i = 0; i < n; i++) {
+            len[i] = cnt[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (len[i] == len[j] + 1) {
+                        cnt[i] += cnt[j];
+                    } else if (len[i] < len[j] + 1) {
+                        len[i] = len[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+                }
+            }
+            if (maxLen == len[i]) {
+                res += cnt[i];
+            } else if (maxLen < len[i]) {
+                maxLen = len[i];
+                res = cnt[i];
+            }
+        }
+        return res;
+    }
+}
