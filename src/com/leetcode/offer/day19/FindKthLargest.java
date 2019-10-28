dd713ab9fd5062fa8d0ca0df6aa34d2288abfdfe
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-28 16:19:55 +0800

    quick select

--- src/com/leetcode/offer/day19/FindKthLargest.java
+++ src/com/leetcode/offer/day19/FindKthLargest.java
@@ -1,7 +1,5 @@
 package com.leetcode.offer.day19;
 
-import java.util.PriorityQueue;
-
 /**
  * @author duson
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
@@ -37,6 +35,65 @@ public class FindKthLargest {
 
         return findK(nums, k - 1, 0, nums.length - 1);
 
+    }
+
+    public int findKthLargest2(int[] nums, int k) {
+        int n = nums.length;
+        int target = n - k;
+        quickSort(nums, 0, n - 1, target);
+        return nums[n - k];
+    }
+
+    private void quickSort(int[] nums, int start, int end, int target) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        int pivot = choosePivot(nums[mid], nums[start], nums[end]);
+        int i = start;
+        int j = end;
+        while (i <= j) {
+            while (nums[i] < pivot) {
+                i++;
+            }
+            while (nums[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                if (nums[i] != nums[j]) {
+                    swap(nums, i, j);
+                }
+                i++;
+                j--;
+            }
+        }
+        if (target <= i - 1) {
+            quickSort(nums, start, i - 1, target);
+        } else {
+            quickSort(nums, i, end, target);
+        }
+    }
+
+    private int choosePivot(int a, int b, int c) {
+        if (a > b) {
+            if (c > a) {
+                return a;
+            } else {
+                return Math.max(c, b);
+            }
+        } else {
+            if (c > b) {
+                return b;
+            } else {
+                return Math.max(c, a);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
     public int findK(int[] nums, int k, int start, int end) {
