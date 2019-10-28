a8ba11dde3b192bc5fa7ba8792d762b828c671c2
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-28 15:01:52 +0800

    wiggle-sort

--- src/com/leetcode/offer/day48/WiggleSort.java
+++ src/com/leetcode/offer/day48/WiggleSort.java
@@ -18,11 +18,10 @@ import java.util.Arrays;
 public class WiggleSort {
 
     public void wiggleSort(int[] nums) {
-        int temp;
         for (int i = 0; i < nums.length - 1; i++) {
             if ((i % 2 == 0 && nums[i] > nums[i + 1])
                     || (i % 2 == 1 && nums[i] < nums[i + 1])) {
-                temp = nums[i + 1];
+                int temp = nums[i + 1];
                 nums[i + 1] = nums[i];
                 nums[i] = temp;
             }
