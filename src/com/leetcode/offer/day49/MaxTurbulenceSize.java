588850c79de96c373d4c3567933c0be60c0097de
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-29 15:59:08 +0800

    longest-turbulent-subarray

---
+++ src/com/leetcode/offer/day49/MaxTurbulenceSize.java
@@ -0,0 +1,58 @@
+package com.leetcode.offer.day49;
+
+/**
+ * @author duson
+ * 当 A 的子数组 A[i], A[i+1], ..., A[j] 满足下列条件时，我们称其为湍流子数组：
+ * <p>
+ * 若 i <= k < j，当 k 为奇数时， A[k] > A[k+1]，且当 k 为偶数时，A[k] < A[k+1]；
+ * 或 若 i <= k < j，当 k 为偶数时，A[k] > A[k+1] ，且当 k 为奇数时， A[k] < A[k+1]。
+ * 也就是说，如果比较符号在子数组中的每个相邻元素对之间翻转，则该子数组是湍流子数组。
+ * <p>
+ * 返回 A 的最大湍流子数组的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[9,4,2,10,7,8,8,1,9]
+ * 输出：5
+ * 解释：(A[1] > A[2] < A[3] > A[4] < A[5])
+ * 示例 2：
+ * <p>
+ * 输入：[4,8,12,16]
+ * 输出：2
+ * 示例 3：
+ * <p>
+ * 输入：[100]
+ * 输出：1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 40000
+ * 0 <= A[i] <= 10^9
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-turbulent-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxTurbulenceSize {
+
+    public int maxTurbulenceSize(int[] a) {
+        int up = 1, down = 1, result = 1;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > a[i - 1]) {
+                up = down + 1;
+                down = 1;
+            } else if (a[i] < a[i - 1]) {
+                down = up + 1;
+                up = 1;
+            } else {
+                up = 1;
+                down = 1;
+            }
+            result = Math.max(result, Math.max(up, down));
+        }
+        return result;
+    }
+}
