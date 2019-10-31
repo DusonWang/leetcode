40240daa3122899c2a2849d5a4358584af3c7cb5
DusongWang <dasong.wds@alibaba-inc.com>
2019-10-31 20:04:00 +0800

    number-of-segments-in-a-string

---
+++ src/com/leetcode/offer/day53/CountSegments.java
@@ -0,0 +1,47 @@
+package com.leetcode.offer.day53;
+
+/**
+ * @author duson
+ * 统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+ * <p>
+ * 请注意，你可以假定字符串里不包括任何不可打印的字符。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: "Hello, my name is John"
+ * 输出: 5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-segments-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CountSegments {
+
+    public int countSegments2(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countSegments(String s) {
+        int count = 0, i = 0, j = 0;
+        while (i < s.length()) {
+            while (i < s.length() && s.charAt(i) == ' ') {
+                i++;
+            }
+            j = i;
+            if (j < s.length()) {
+                while (j < s.length() && s.charAt(j) != ' ') {
+                    j++;
+                }
+                count++;
+                i = j;
+            }
+        }
+        return count;
+    }
+}
