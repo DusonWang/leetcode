b770f3e895792c561d35185a35a6f40d64468626
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-02 23:01:42 +0800

    majority-element-ii

---
+++ src/com/leetcode/offer/day56/MajorityElement.java
@@ -0,0 +1,71 @@
+package com.leetcode.offer.day56;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author duson
+ * 给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+ * <p>
+ * 说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,3]
+ * 输出: [3]
+ * 示例 2:
+ * <p>
+ * 输入: [1,1,1,3,3,2,2,2]
+ * 输出: [1,2]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MajorityElement {
+
+    public List<Integer> majorityElement(int[] nums) {
+        int candidate1 = 0;
+        int candidate2 = 1;
+        int count1 = 0;
+        int count2 = 0;
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0; count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length/3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length/3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
