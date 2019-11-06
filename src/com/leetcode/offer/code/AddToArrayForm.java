2e2ffb0af161a6126d82ed59cf269aaaddd1f816
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-06 14:33:02 +0800

    add-to-array-form-of-integer

---
+++ src/com/leetcode/offer/code/AddToArrayForm.java
@@ -0,0 +1,67 @@
+package com.leetcode.offer.code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author duson
+ * 对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。
+ * <p>
+ * 给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1,2,0,0], K = 34
+ * 输出：[1,2,3,4]
+ * 解释：1200 + 34 = 1234
+ * 解释 2：
+ * <p>
+ * 输入：A = [2,7,4], K = 181
+ * 输出：[4,5,5]
+ * 解释：274 + 181 = 455
+ * 示例 3：
+ * <p>
+ * 输入：A = [2,1,5], K = 806
+ * 输出：[1,0,2,1]
+ * 解释：215 + 806 = 1021
+ * 示例 4：
+ * <p>
+ * 输入：A = [9,9,9,9,9,9,9,9,9,9], K = 1
+ * 输出：[1,0,0,0,0,0,0,0,0,0,0]
+ * 解释：9999999999 + 1 = 10000000000
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 10000
+ * 0 <= A[i] <= 9
+ * 0 <= K <= 10000
+ * 如果 A.length > 1，那么 A[0] != 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-to-array-form-of-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class AddToArrayForm {
+
+    public List<Integer> addToArrayForm(int[] a, int k) {
+        int n = a.length;
+        int cur = k;
+        List<Integer> ans = new ArrayList<>();
+
+        int i = n;
+        while (--i >= 0 || cur > 0) {
+            if (i >= 0) {
+                cur += a[i];
+            }
+            ans.add(cur % 10);
+            cur /= 10;
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
