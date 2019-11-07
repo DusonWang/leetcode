7d0d9aa32a088e2f72a52e73bad1cea57a2f4280
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-07 15:17:55 +0800

    max-area-of-island

---
+++ src/com/leetcode/offer/code/MaxAreaOfIsland.java
@@ -0,0 +1,47 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ * <p>
+ * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ * <p>
+ * Example 1:
+ * <p>
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+ * Example 2:
+ * <p>
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ * Note: The length of each dimension in the given grid does not exceed 50.
+ */
+public class MaxAreaOfIsland {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, areaOfIsland(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int areaOfIsland(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i, j - 1) + areaOfIsland(grid, i, j + 1);
+        }
+        return 0;
+    }
+}
