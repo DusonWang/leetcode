6041fec35887fc5001528dadffef40d2a2391c15
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-09 15:46:32 +0800

    contiguous-array

---
+++ src/com/leetcode/offer/code/FindMaxLength.java
@@ -0,0 +1,54 @@
+package com.leetcode.offer.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duson
+ * 给定一个二进制数组, 找到含有相同数量的 0 和 1 的最长连续子数组（的长度）。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [0,1]
+ * 输出: 2
+ * 说明: [0, 1] 是具有相同数量0和1的最长连续子数组。
+ * 示例 2:
+ * <p>
+ * 输入: [0,1,0]
+ * 输出: 2
+ * 说明: [0, 1] (或 [1, 0]) 是具有相同数量0和1的最长连续子数组。
+ *  
+ * <p>
+ * 注意: 给定的二进制数组的长度不会超过50000。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contiguous-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindMaxLength {
+
+    public int findMaxLength(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                nums[i] = -1;
+            }
+        }
+
+        Map<Integer, Integer> sumToIndex = new HashMap<>();
+        sumToIndex.put(0, -1);
+        int sum = 0, max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sumToIndex.containsKey(sum)) {
+                max = Math.max(max, i - sumToIndex.get(sum));
+            } else {
+                sumToIndex.put(sum, i);
+            }
+        }
+
+        return max;
+    }
+}
