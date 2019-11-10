c38f58d617f7dec6ca372525e9030a04a25d6689
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-10 20:16:25 +0800

    minimum-absolute-difference-in-bst

---
+++ src/com/leetcode/offer/code/GetMinimumDifference.java
@@ -0,0 +1,60 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * <p>
+ * 给定一个所有节点为非负值的二叉搜索树，求树中任意两节点的差的绝对值的最小值。
+ * <p>
+ * 示例 :
+ * <p>
+ * 输入:
+ * <p>
+ * 1
+ * \
+ * 3
+ * /
+ * 2
+ * <p>
+ * 输出:
+ * 1
+ * <p>
+ * 解释:
+ * 最小绝对差为1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+ * 注意: 树中至少有2个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GetMinimumDifference {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private int min = Integer.MAX_VALUE;
+    private Integer prev = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) {
+            return min;
+        }
+
+        getMinimumDifference(root.left);
+
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+        prev = root.val;
+
+        getMinimumDifference(root.right);
+
+        return min;
+    }
+}
