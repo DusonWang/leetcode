f4a107d2561cb4892e28c4a607dfbdd3dc7ebe80
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-12 23:23:18 +0800

    isomorphic-strings

---
+++ src/com/leetcode/offer/code/IsIsomorphic.java
@@ -0,0 +1,43 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 给定两个字符串 s 和 t，判断它们是否是同构的。
+ * <p>
+ * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+ * <p>
+ * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "egg", t = "add"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s = "foo", t = "bar"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: s = "paper", t = "title"
+ * 输出: true
+ * 说明:
+ * 你可以假设 s 和 t 具有相同的长度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/isomorphic-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsIsomorphic {
+
+    public boolean isIsomorphic(String s, String t) {
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.indexOf(ch1[i]) != t.indexOf(ch2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
