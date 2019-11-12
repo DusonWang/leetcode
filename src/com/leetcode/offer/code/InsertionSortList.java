27b9d763441dea572b3adbf5ae16b69bb56cfaaf
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-12 15:57:22 +0800

    insertion-sort-list

---
+++ src/com/leetcode/offer/code/InsertionSortList.java
@@ -0,0 +1,66 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 对链表进行插入排序。
+ * <p>
+ * <p>
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ * <p>
+ *  
+ * <p>
+ * 插入排序算法：
+ * <p>
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2：
+ * <p>
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insertion-sort-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class InsertionSortList {
+
+    public class ListNode {
+        int val;
+
+        ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode helper = new ListNode(0);
+        ListNode cur = head;
+        ListNode pre = helper;
+        ListNode next;
+        while (cur != null) {
+            next = cur.next;
+            while (pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = helper;
+            cur = next;
+        }
+        return helper.next;
+    }
+}
