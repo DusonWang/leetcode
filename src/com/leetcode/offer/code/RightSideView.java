a93fe6cff63bc673021cb018cd0b67dacdd1fe46
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-13 13:44:58 +0800

    binary-tree-right-side-view 2

--- src/com/leetcode/offer/code/RightSideView.java
+++ src/com/leetcode/offer/code/RightSideView.java
@@ -1,7 +1,9 @@
 package com.leetcode.offer.code;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author duson
@@ -34,6 +36,32 @@ public class RightSideView {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return res;
     }
 
     public List<Integer> rightSideView(TreeNode root) {
