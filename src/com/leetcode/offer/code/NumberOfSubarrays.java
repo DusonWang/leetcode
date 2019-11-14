004e5e1a79b14bdbfd9828773956d068f0610c6f
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-14 21:24:08 +0800

    fun2

--- src/com/leetcode/offer/code/NumberOfSubarrays.java
+++ src/com/leetcode/offer/code/NumberOfSubarrays.java
@@ -39,6 +39,24 @@ package com.leetcode.offer.code;
  */
 public class NumberOfSubarrays {
 
+    public int numberOfSubarrays2(int[] nums, int k) {
+        int len = nums.length;
+        int res = 0;
+        int feed = 0;
+        int[] arr = new int[len + 2];
+        for (int i = 0; i < len; i++) {
+            if ((nums[i] & 1) == 1) {
+                arr[++feed] = i;
+            }
+        }
+        arr[0] = -1;
+        arr[feed + 1] = len;
+        for (int i = 1; i + k < feed + 2; i++) {
+            res += (arr[i] - arr[i - 1]) * (arr[i + k] - arr[i + k - 1]);
+        }
+        return res;
+    }
+
     public int numberOfSubarrays(int[] nums, int k) {
         int s = 0;
         int[] d = new int[50000];
