4ae0d947f78f141edbb5e1eddea284ea7e0df9f1
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-21 14:33:42 +0800

    trapping-rain-water-ii

---
+++ src/com/leetcode/offer/code/TrapRainWater.java
@@ -0,0 +1,100 @@
+package com.leetcode.offer.code;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author duson
+ * <p>
+ * 给定一个 m x n 的矩阵，其中的值均为正整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+ * <p>
+ *  
+ * <p>
+ * 说明:
+ * <p>
+ * m 和 n 都是小于110的整数。每一个单位的高度都大于 0 且小于 20000。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给出如下 3x6 的高度图:
+ * [
+ * [1,4,3,1,3,2],
+ * [3,2,1,3,2,4],
+ * [2,3,3,2,3,1]
+ * ]
+ * <p>
+ * 返回 4。
+ * <p>
+ * <p>
+ * 如上图所示，这是下雨前的高度图[[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] 的状态。
+ * <p>
+ *  
+ * <p>
+ * <p>
+ * <p>
+ * 下雨后，雨水将会被存储在这些方块中。总的接雨水量是4。
+ * <p>
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/trapping-rain-water-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TrapRainWater {
+
+    public static class Cell {
+        int row;
+        int col;
+        int height;
+
+        public Cell(int row, int col, int height) {
+            this.row = row;
+            this.col = col;
+            this.height = height;
+        }
+    }
+
+    public int trapRainWater(int[][] heights) {
+        if (heights == null || heights.length == 0 || heights[0].length == 0) {
+            return 0;
+        }
+
+        PriorityQueue<Cell> queue = new PriorityQueue<>(1, Comparator.comparingInt(a -> a.height));
+
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+            queue.offer(new Cell(i, 0, heights[i][0]));
+            queue.offer(new Cell(i, n - 1, heights[i][n - 1]));
+        }
+
+        for (int i = 0; i < n; i++) {
+            visited[0][i] = true;
+            visited[m - 1][i] = true;
+            queue.offer(new Cell(0, i, heights[0][i]));
+            queue.offer(new Cell(m - 1, i, heights[m - 1][i]));
+        }
+
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int res = 0;
+        while (!queue.isEmpty()) {
+            Cell cell = queue.poll();
+            for (int[] dir : dirs) {
+                int row = cell.row + dir[0];
+                int col = cell.col + dir[1];
+                if (row >= 0 && row < m && col >= 0 && col < n && !visited[row][col]) {
+                    visited[row][col] = true;
+                    res += Math.max(0, cell.height - heights[row][col]);
+                    queue.offer(new Cell(row, col, Math.max(heights[row][col], cell.height)));
+                }
+            }
+        }
+        return res;
+    }
+}
