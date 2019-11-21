99139194ca001e329a3ce3a2261fdbd2a76f8de1
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-21 16:02:05 +0800

    longest-word-in-dictionary

---
+++ src/com/leetcode/offer/code/LongestWord.java
@@ -0,0 +1,61 @@
+package com.leetcode.offer.code;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author duson
+ * <p>
+ * 给出一个字符串数组words组成的一本英语词典。从中找出最长的一个单词，该单词是由words词典中其他单词逐步添加一个字母组成。若其中有多个可行的答案，则返回答案中字典序最小的单词。
+ * <p>
+ * 若无答案，则返回空字符串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * words = ["w","wo","wor","worl", "world"]
+ * 输出: "world"
+ * 解释:
+ * 单词"world"可由"w", "wo", "wor", 和 "worl"添加一个字母组成。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+ * 输出: "apple"
+ * 解释:
+ * "apply"和"apple"都能由词典中的单词组成。但是"apple"得字典序小于"apply"。
+ * 注意:
+ * <p>
+ * 所有输入的字符串都只包含小写字母。
+ * words数组长度范围为[1,1000]。
+ * words[i]的长度范围为[1,30]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-word-in-dictionary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestWord {
+
+    public String longestWord(String[] words) {
+        String ans = "";
+        Set<String> wordset = new HashSet<>();
+        Collections.addAll(wordset, words);
+        for (String word : words) {
+            if (word.length() > ans.length() ||
+                    word.length() == ans.length() && word.compareTo(ans) < 0) {
+                boolean good = true;
+                for (int k = 1; k < word.length(); ++k) {
+                    if (!wordset.contains(word.substring(0, k))) {
+                        good = false;
+                        break;
+                    }
+                }
+                if (good) {
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+    }
+}
