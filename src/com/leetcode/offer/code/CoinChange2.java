290b18a67deb8835348a876fdf7c9c1a97fa32b4
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-25 19:24:08 +0800

    coin-change-2

---
+++ src/com/leetcode/offer/code/CoinChange2.java
@@ -0,0 +1,59 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * <p>
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。 
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: amount = 5, coins = [1, 2, 5]
+ * 输出: 4
+ * 解释: 有四种方式可以凑成总金额:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * 示例 2:
+ * <p>
+ * 输入: amount = 3, coins = [2]
+ * 输出: 0
+ * 解释: 只用面额2的硬币不能凑成总金额3。
+ * 示例 3:
+ * <p>
+ * 输入: amount = 10, coins = [10]
+ * 输出: 1
+ *  
+ * <p>
+ * 注意:
+ * <p>
+ * 你可以假设：
+ * <p>
+ * 0 <= amount (总金额) <= 5000
+ * 1 <= coin (硬币面额) <= 5000
+ * 硬币种类不超过 500 种
+ * 结果符合 32 位符号整数
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change-2
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CoinChange2 {
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        int len = coins.length;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j - coins[i - 1] >= 0) {
+                    dp[j] += dp[j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
