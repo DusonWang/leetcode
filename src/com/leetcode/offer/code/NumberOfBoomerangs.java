e057989b39f10256150efb1df5875b3498200455
DusongWang <dasong.wds@alibaba-inc.com>
2019-11-25 13:15:52 +0800

    number-of-boomerangs

---
+++ src/com/leetcode/offer/code/NumberOfBoomerangs.java
@@ -0,0 +1,57 @@
+package com.leetcode.offer.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duson
+ * <p>
+ * 给定平面上 n 对不同的点，“回旋镖” 是由点表示的元组 (i, j, k) ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+ * <p>
+ * 找到所有回旋镖的数量。你可以假设 n 最大为 500，所有点的坐标在闭区间 [-10000, 10000] 中。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * [[0,0],[1,0],[2,0]]
+ * <p>
+ * 输出:
+ * 2
+ * <p>
+ * 解释:
+ * 两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]]
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-boomerangs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumberOfBoomerangs {
+
+    public int numberOfBoomerangs(int[][] points) {
+        int len = points.length;
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int d = getDistance(points[i], points[j]);
+                map.put(d, map.getOrDefault(d, 0) + 1);
+            }
+            for (int val : map.values()) {
+                result += val * (val - 1);
+            }
+            map.clear();
+        }
+        return result;
+    }
+
+    private int getDistance(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+
+        return dx * dx + dy * dy;
+    }
+}
