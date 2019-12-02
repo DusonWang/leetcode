89c808d517819356524d73bb6fa7dd3ef304f2cb
DusongWang <dasong.wds@alibaba-inc.com>
2019-12-02 16:47:20 +0800

    minimum-moves-to-equal-array-elements-ii

---
+++ src/com/leetcode/offer/code/MinMoves2.java
@@ -0,0 +1,37 @@
+package com.leetcode.offer.code;
+
+import java.util.Arrays;
+
+/**
+ * @author duson
+ * 给定一个非空整数数组，找到使所有数组元素相等所需的最小移动数，其中每次移动可将选定的一个元素加1或减1。 您可以假设数组的长度最多为10000。
+ * <p>
+ * 例如:
+ * <p>
+ * 输入:
+ * [1,2,3]
+ * <p>
+ * 输出:
+ * 2
+ * <p>
+ * 说明：
+ * 只有两个动作是必要的（记得每一步仅可使其中一个元素加1或减1）：
+ * <p>
+ * [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinMoves2 {
+
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int num : nums) {
+            sum += Math.abs(nums[nums.length / 2] - num);
+        }
+        return sum;
+    }
+}
