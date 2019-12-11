25a3e5f1b479a5ee9efa2bed51aee6320596a1bf
王大松 <dasong.wds@alibaba-inc.com>
2019-12-11 14:46:21 +0800

    map-sum-pairs

---
+++ src/com/leetcode/offer/code/MapSum.java
@@ -0,0 +1,48 @@
+package com.leetcode.offer.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duson
+ * <p>
+ * 实现一个 MapSum 类里的两个方法，insert 和 sum。
+ * <p>
+ * 对于方法 insert，你将得到一对（字符串，整数）的键值对。字符串表示键，整数表示值。如果键已经存在，那么原来的键值对将被替代成新的键值对。
+ * <p>
+ * 对于方法 sum，你将得到一个表示前缀的字符串，你需要返回所有以该前缀开头的键的值的总和。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: insert("apple", 3), 输出: Null
+ * 输入: sum("ap"), 输出: 3
+ * 输入: insert("app", 2), 输出: Null
+ * 输入: sum("ap"), 输出: 5
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/map-sum-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MapSum {
+
+    private Map<String, Integer> map;
+
+    public MapSum() {
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+
+    public int sum(String prefix) {
+        int res = 0;
+        for (String mapPrefix : map.keySet()) {
+            if (mapPrefix.startsWith(prefix)) {
+                res += map.get(mapPrefix);
+            }
+        }
+        return res;
+    }
+}
