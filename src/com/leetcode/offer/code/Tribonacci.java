e4b600dd5868a1a5dabd2b0aed09d29ac2a8c082
王大松 <dasong.wds@alibaba-inc.com>
2019-12-14 22:08:35 +0800

    n-th-tribonacci-number

---
+++ src/com/leetcode/offer/code/Tribonacci.java
@@ -0,0 +1,54 @@
+package com.leetcode.offer.code;
+
+/**
+ * 泰波那契序列 Tn 定义如下： 
+ * <p>
+ * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+ * <p>
+ * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 4
+ * 输出：4
+ * 解释：
+ * T_3 = 0 + 1 + 1 = 2
+ * T_4 = 1 + 1 + 2 = 4
+ * 示例 2：
+ * <p>
+ * 输入：n = 25
+ * 输出：1389537
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= n <= 37
+ * 答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-th-tribonacci-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Tribonacci {
+
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+}
