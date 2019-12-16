298ebeee898a71b2f7cd303f895d0836dec1bacd
王大松 <dasong.wds@alibaba-inc.com>
2019-12-16 22:22:39 +0800

    validate-stack-sequences

---
+++ src/com/leetcode/offer/code/ValidateStackSequences.java
@@ -0,0 +1,52 @@
+package com.leetcode.offer.code;
+
+import java.util.Stack;
+
+/**
+ * @author duson
+ * 给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * 输出：true
+ * 解释：我们可以按以下顺序执行：
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * 示例 2：
+ * <p>
+ * 输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * 输出：false
+ * 解释：1 不能在 2 之前弹出。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= pushed.length == popped.length <= 1000
+ * 0 <= pushed[i], popped[i] < 1000
+ * pushed 是 popped 的排列。
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-stack-sequences
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ValidateStackSequences {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0, j = 0, len = pushed.length;
+        Stack<Integer> s = new Stack<>();
+        while (i < len) {
+            while (i < len && (s.isEmpty() || s.peek() != popped[j])) {
+                s.push(pushed[i++]);
+            }
+            while (!s.isEmpty() && s.peek() == popped[j]) {
+                s.pop();
+                j++;
+            }
+        }
+        return j == len;
+    }
+}
