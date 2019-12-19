f254b5f5890bfd347c7ed97b326c529a473d13d4
王大松 <dasong.wds@alibaba-inc.com>
2019-12-19 23:30:05 +0800

    subarray-sums-divisible-by-k

---
+++ src/com/leetcode/offer/code/SubarraysDivByK.java
@@ -0,0 +1,47 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：A = [4,5,0,-2,-3,1], K = 5
+ * 输出：7
+ * 解释：
+ * 有 7 个子数组满足其元素之和可被 K = 5 整除：
+ * [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 30000
+ * -10000 <= A[i] <= 10000
+ * 2 <= K <= 10000
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subarray-sums-divisible-by-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SubarraysDivByK {
+
+    public int subarraysDivByK(int[] a, int k) {
+        int[] modCount = new int[k];
+        modCount[0] = 1;
+        int sum = 0;
+        int result = 0;
+        for (int num : a) {
+            sum += num;
+            int remainder = sum % k;
+            if (remainder < 0) {
+                remainder += k;
+            }
+            result += modCount[remainder];
+            modCount[remainder]++;
+        }
+        return result;
+    }
+}
