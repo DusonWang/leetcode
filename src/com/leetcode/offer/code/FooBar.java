d24fbd5375c29bc45bf8128aa1a03f0669ea7386
DusongWang <dasong.wds@alibaba-inc.com>
2019-12-28 22:16:44 +0800

    print-foobar-alternately

---
+++ src/com/leetcode/offer/code/FooBar.java
@@ -0,0 +1,70 @@
+package com.leetcode.offer.code;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author duson
+ * 我们提供一个类：
+ * <p>
+ * class FooBar {
+ * public void foo() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("foo");
+ *     }
+ * }
+ * <p>
+ * public void bar() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("bar");
+ *     }
+ * }
+ * }
+ * 两个不同的线程将会共用一个 FooBar 实例。其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+ * <p>
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: n = 1
+ * 输出: "foobar"
+ * 解释: 这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+ * 示例 2:
+ * <p>
+ * 输入: n = 2
+ * 输出: "foobarfoobar"
+ * 解释: "foobar" 将被输出两次。
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/print-foobar-alternately
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FooBar {
+
+    private int n;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    private Semaphore foo = new Semaphore(1);
+    private Semaphore bar = new Semaphore(0);
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            foo.acquire();
+            printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            bar.acquire();
+            printBar.run();
+            foo.release();
+        }
+    }
+}
