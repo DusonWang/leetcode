95f8df9aad3d63aa25fece6e60712b98deddef16
DusongWang <dasong.wds@alibaba-inc.com>
2020-01-05 12:53:59 +0800

    k-th-symbol-in-grammar

---
+++ src/com/leetcode/offer/code/KthGrammar.java
@@ -0,0 +1,46 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * <p>
+ * 在第一行我们写上一个 0。接下来的每一行，将前一行中的0替换为01，1替换为10。
+ * <p>
+ * 给定行数 N 和序数 K，返回第 N 行中第 K个字符。（K从1开始）
+ * <p>
+ * <p>
+ * 例子:
+ * <p>
+ * 输入: N = 1, K = 1
+ * 输出: 0
+ * <p>
+ * 输入: N = 2, K = 1
+ * 输出: 0
+ * <p>
+ * 输入: N = 2, K = 2
+ * 输出: 1
+ * <p>
+ * 输入: N = 4, K = 5
+ * 输出: 1
+ * <p>
+ * 解释:
+ * 第一行: 0
+ * 第二行: 01
+ * 第三行: 0110
+ * 第四行: 01101001
+ * <p>
+ * 注意：
+ * <p>
+ * N 的范围 [1, 30].
+ * K 的范围 [1, 2^(N-1)].
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/k-th-symbol-in-grammar
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class KthGrammar {
+
+    public int kthGrammar(int n, int k) {
+        return Integer.bitCount(k - 1) % 2;
+    }
+}
