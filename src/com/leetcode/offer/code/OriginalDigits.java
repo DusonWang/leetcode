c2fa38d2b16bd8f37eb478e1cca002b2e33eac66
DusongWang <dasong.wds@alibaba-inc.com>
2020-01-09 20:19:14 +0800

    reconstruct-original-digits-from-english

--- src/com/leetcode/offer/code/OriginalDigits.java
+++ src/com/leetcode/offer/code/OriginalDigits.java
@@ -28,51 +28,42 @@ package com.leetcode.offer.code;
 public class OriginalDigits {
 
     public String originalDigits(String s) {
-        int[] count = new int[10];
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'z') {
-                count[0]++;
-            }
-            if (c == 'w') {
-                count[2]++;
-            }
-            if (c == 'x') {
-                count[6]++;
-            }
-            if (c == 's') {
-                count[7]++;
-            }
-            if (c == 'g') {
-                count[8]++;
-            }
-            if (c == 'u') {
-                count[4]++;
-            }
-            if (c == 'f') {
-                count[5]++;
-            }
-            if (c == 'h') {
-                count[3]++;
-            }
-            if (c == 'i') {
-                count[9]++;
-            }
-            if (c == 'o') {
-                count[1]++;
+        // building hashmap letter -> its frequency
+        char[] count = new char[26 + (int) 'a'];
+        for (char letter : s.toCharArray()) {
+            count[letter]++;
+        }
+
+        // building hashmap digit -> its frequency
+        int[] out = new int[10];
+        // letter "z" is present only in "zero"
+        out[0] = count['z'];
+        // letter "w" is present only in "two"
+        out[2] = count['w'];
+        // letter "u" is present only in "four"
+        out[4] = count['u'];
+        // letter "x" is present only in "six"
+        out[6] = count['x'];
+        // letter "g" is present only in "eight"
+        out[8] = count['g'];
+        // letter "h" is present only in "three" and "eight"
+        out[3] = count['h'] - out[8];
+        // letter "f" is present only in "five" and "four"
+        out[5] = count['f'] - out[4];
+        // letter "s" is present only in "seven" and "six"
+        out[7] = count['s'] - out[6];
+        // letter "i" is present in "nine", "five", "six", and "eight"
+        out[9] = count['i'] - out[5] - out[6] - out[8];
+        // letter "n" is present in "one", "nine", and "seven"
+        out[1] = count['n'] - out[7] - 2 * out[9];
+
+        // building output string
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < out[i]; j++) {
+                output.append(i);
             }
         }
-        count[7] -= count[6];
-        count[5] -= count[4];
-        count[3] -= count[8];
-        count[9] = count[9] - count[8] - count[5] - count[6];
-        count[1] = count[1] - count[0] - count[2] - count[4];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= 9; i++) {
-            for (int j = 0; j < count[i]; j++) {
-                sb.append(i);
-            }
-        }
-        return sb.toString();
+        return output.toString();
     }
 }
