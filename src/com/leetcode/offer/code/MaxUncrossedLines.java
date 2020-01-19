b3f660873fd284af4099ac7b0a09cfc532e0dd41
DusongWang <dasong.wds@alibaba-inc.com>
2020-01-19 17:48:22 +0800

    uncrossed-lines

---
+++ src/com/leetcode/offer/code/MaxUncrossedLines.java
@@ -0,0 +1,62 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 我们在两条独立的水平线上按给定的顺序写下 A 和 B 中的整数。
+ * <p>
+ * 现在，我们可以绘制一些连接两个数字 A[i] 和 B[j] 的直线，只要 A[i] == B[j]，且我们绘制的直线不与任何其他连线（非水平线）相交。
+ * <p>
+ * 以这种方法绘制线条，并返回我们可以绘制的最大连线数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：A = [1,4,2], B = [1,2,4]
+ * 输出：2
+ * 解释：
+ * 我们可以画出两条不交叉的线，如上图所示。
+ * 我们无法画出第三条不相交的直线，因为从 A[1]=4 到 B[2]=4 的直线将与从 A[2]=2 到 B[1]=2 的直线相交。
+ * 示例 2：
+ * <p>
+ * 输入：A = [2,5,1,2,5], B = [10,5,2,1,5,2]
+ * 输出：3
+ * 示例 3：
+ * <p>
+ * 输入：A = [1,3,7,1,7,5], B = [1,9,2,5,1]
+ * 输出：2
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 500
+ * 1 <= B.length <= 500
+ * 1 <= A[i], B[i] <= 2000
+ *  
+ * <p>
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/uncrossed-lines
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxUncrossedLines {
+
+    public int maxUncrossedLines(int[] a, int[] b) {
+        int n1 = a.length;
+        int n2 = b.length;
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (a[i] == b[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+}
