92d65d6741d65fe5b80fd633a644a2529c6fc9ae
DusongWang <dasong.wds@alibaba-inc.com>
2020-01-19 13:55:56 +0800

    maximum-69-number

---
+++ src/com/leetcode/offer/code/Maximum69Number.java
@@ -0,0 +1,57 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 给你一个仅由数字 6 和 9 组成的正整数 num。
+ * <p>
+ * 你最多只能翻转一位数字，将 6 变成 9，或者把 9 变成 6 。
+ * <p>
+ * 请返回你可以得到的最大数字。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：num = 9669
+ * 输出：9969
+ * 解释：
+ * 改变第一位数字可以得到 6669 。
+ * 改变第二位数字可以得到 9969 。
+ * 改变第三位数字可以得到 9699 。
+ * 改变第四位数字可以得到 9666 。
+ * 其中最大的数字是 9969 。
+ * 示例 2：
+ * <p>
+ * 输入：num = 9996
+ * 输出：9999
+ * 解释：将最后一位从 6 变到 9，其结果 9999 是最大的数。
+ * 示例 3：
+ * <p>
+ * 输入：num = 9999
+ * 输出：9999
+ * 解释：无需改变就已经是最大的数字了。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= num <= 10^4
+ * num 每一位上的数字都是 6 或者 9 。
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-69-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Maximum69Number {
+
+    public int maximum69Number(int num) {
+        char[] chars = Integer.toString(num).toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(chars));
+    }
+}
