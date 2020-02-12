3e72d6f9f2bf8b35d890895d5602c6fed8b1e555
DusongWang <dasong.wds@alibaba-inc.com>
2020-02-12 21:41:30 +0800

    ti-huan-kong-ge-lcof

---
+++ src/com/leetcode/offer/code/ReplaceSpace.java
@@ -0,0 +1,38 @@
+package com.leetcode.offer.code;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= s 的长度 <= 10000
+ * <p>
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ReplaceSpace {
+
+    public String replaceSpace(String s) {
+        char[] a = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : a) {
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
