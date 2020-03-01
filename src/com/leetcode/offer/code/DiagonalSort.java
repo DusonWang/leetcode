684306513ea521c8758579cb9fca4bdf00ac73cd
DusongWang <dasong.wds@alibaba-inc.com>
2020-03-01 19:55:58 +0800

    sort-the-matrix-diagonally

---
+++ src/com/leetcode/offer/code/DiagonalSort.java
@@ -0,0 +1,51 @@
+package com.leetcode.offer.code;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+/**
+ * @author duson
+ * 给你一个 m * n 的整数矩阵 mat ，请你将同一条对角线上的元素（从左上到右下）按升序排序后，返回排好序的矩阵。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+ * 输出：[[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 100
+ * 1 <= mat[i][j] <= 100
+ * 通过次数1,187提交次数1,590
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-the-matrix-diagonally
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DiagonalSort {
+
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d.putIfAbsent(i - j, new PriorityQueue<>());
+                d.get(i - j).add(mat[i][j]);
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mat[i][j] = d.get(i - j).poll();
+            }
+        }
+        return mat;
+    }
+}
