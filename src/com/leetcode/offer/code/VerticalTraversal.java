5748024697c5e4fb7442309e46cccff42fedb083
DusongWang <dasong.wds@alibaba-inc.com>
2020-03-02 20:24:42 +0800

    vertical-order-traversal-of-a-binary-tree

---
+++ src/com/leetcode/offer/code/VerticalTraversal.java
@@ -0,0 +1,97 @@
+package com.leetcode.offer.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+/**
+ * @author duson
+ * 给定二叉树，按垂序遍历返回其结点值。
+ * <p>
+ * 对位于 (X, Y) 的每个结点而言，其左右子结点分别位于 (X-1, Y-1) 和 (X+1, Y-1)。
+ * <p>
+ * 把一条垂线从 X = -infinity 移动到 X = +infinity ，每当该垂线与结点接触时，我们按从上到下的顺序报告结点的值（ Y 坐标递减）。
+ * <p>
+ * 如果两个结点位置相同，则首先报告的结点值较小。
+ * <p>
+ * 按 X 坐标顺序返回非空报告的列表。每个报告都有一个结点值列表。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[3,9,20,null,null,15,7]
+ * 输出：[[9],[3,15],[20],[7]]
+ * 解释：
+ * 在不丧失其普遍性的情况下，我们可以假设根结点位于 (0, 0)：
+ * 然后，值为 9 的结点出现在 (-1, -1)；
+ * 值为 3 和 15 的两个结点分别出现在 (0, 0) 和 (0, -2)；
+ * 值为 20 的结点出现在 (1, -1)；
+ * 值为 7 的结点出现在 (2, -2)。
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[1,2,3,4,5,6,7]
+ * 输出：[[4],[2],[1,5,6],[3],[7]]
+ * 解释：
+ * 根据给定的方案，值为 5 和 6 的两个结点出现在同一位置。
+ * 然而，在报告 "[1,5,6]" 中，结点值 5 排在前面，因为 5 小于 6。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 树的结点数介于 1 和 1000 之间。
+ * 每个结点值介于 0 和 1000 之间。
+ * 通过次数1,885提交次数4,967
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/vertical-order-traversal-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class VerticalTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        dfs(root, 0, 0, map);
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            list.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.get(list.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return list;
+    }
+
+    private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+        if (root == null) {
+            return;
+        }
+        if (!map.containsKey(x)) {
+            map.put(x, new TreeMap<>());
+        }
+        if (!map.get(x).containsKey(y)) {
+            map.get(x).put(y, new PriorityQueue<>());
+        }
+        map.get(x).get(y).offer(root.val);
+        dfs(root.left, x - 1, y + 1, map);
+        dfs(root.right, x + 1, y + 1, map);
+    }
+}
