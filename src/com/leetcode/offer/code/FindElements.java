8637bb3e87e4a50a04e4f404cb69b6121cee47c6
DusongWang <dasong.wds@alibaba-inc.com>
2020-03-21 12:13:52 +0800

    FindElements

---
+++ src/com/leetcode/offer/code/FindElements.java
@@ -0,0 +1,113 @@
+package com.leetcode.offer.code;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author duson
+ * 给出一个满足下述规则的二叉树：
+ *
+ * root.val == 0
+ * 如果 treeNode.val == x 且 treeNode.left != null，那么 treeNode.left.val == 2 * x + 1
+ * 如果 treeNode.val == x 且 treeNode.right != null，那么 treeNode.right.val == 2 * x + 2
+ * 现在这个二叉树受到「污染」，所有的 treeNode.val 都变成了 -1。
+ *
+ * 请你先还原二叉树，然后实现 FindElements 类：
+ *
+ * FindElements(TreeNode* root) 用受污染的二叉树初始化对象，你需要先把它还原。
+ * bool find(int target) 判断目标值 target 是否存在于还原后的二叉树中并返回结果。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：
+ * ["FindElements","find","find"]
+ * [[[-1,null,-1]],[1],[2]]
+ * 输出：
+ * [null,false,true]
+ * 解释：
+ * FindElements findElements = new FindElements([-1,null,-1]);
+ * findElements.find(1); // return False
+ * findElements.find(2); // return True
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：
+ * ["FindElements","find","find","find"]
+ * [[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+ * 输出：
+ * [null,true,true,false]
+ * 解释：
+ * FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+ * findElements.find(1); // return True
+ * findElements.find(3); // return True
+ * findElements.find(5); // return False
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：
+ * ["FindElements","find","find","find","find"]
+ * [[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+ * 输出：
+ * [null,true,false,false,true]
+ * 解释：
+ * FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+ * findElements.find(2); // return True
+ * findElements.find(3); // return False
+ * findElements.find(4); // return False
+ * findElements.find(5); // return True
+ *
+ *
+ * 提示：
+ *
+ * TreeNode.val == -1
+ * 二叉树的高度不超过 20
+ * 节点的总数在 [1, 10^4] 之间
+ * 调用 find() 的总次数在 [1, 10^4] 之间
+ * 0 <= target <= 10^6
+ * 通过次数2,736提交次数3,902
+ * 在真实的面试中遇到过这道题？
+ */
+public class FindElements {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private Set<Integer> set;
+
+    public FindElements(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        set = new HashSet<>();
+        root.val = 0;
+        set.add(root.val);
+        solution(root.left, root, true);
+        solution(root.right, root, false);
+    }
+
+    public void solution(TreeNode root, TreeNode pre, boolean dist) {
+        if(root == null){
+            return;
+        }
+        root.val = 2 * pre.val + (dist ? 1 : 2);
+        set.add(root.val);
+        solution(root.left, root, true);
+        solution(root.right, root, false);
+    }
+
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+}
