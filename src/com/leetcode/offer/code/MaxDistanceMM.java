85efc8e7de2a75c7d15e935e9d15425a048419a7
DusongWang <dasong.wds@alibaba-inc.com>
2020-03-24 23:06:14 +0800

    as-far-from-land-as-possible

---
+++ src/com/leetcode/offer/code/MaxDistanceMM.java
@@ -0,0 +1,81 @@
+package com.leetcode.offer.code;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author duson
+ * 你现在手里有一份大小为 N x N 的『地图』（网格） grid，上面的每个『区域』（单元格）都用 0 和 1 标记好了。其中 0 代表海洋，1 代表陆地，你知道距离陆地区域最远的海洋区域是是哪一个吗？请返回该海洋区域到离它最近的陆地区域的距离。
+ * <p>
+ * 我们这里说的距离是『曼哈顿距离』（ Manhattan Distance）：(x0, y0) 和 (x1, y1) 这两个区域之间的距离是 |x0 - x1| + |y0 - y1| 。
+ * <p>
+ * 如果我们的地图上只有陆地或者海洋，请返回 -1。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[[1,0,1],[0,0,0],[1,0,1]]
+ * 输出：2
+ * 解释：
+ * 海洋区域 (1, 1) 和所有陆地区域之间的距离都达到最大，最大距离为 2。
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：[[1,0,0],[0,0,0],[0,0,0]]
+ * 输出：4
+ * 解释：
+ * 海洋区域 (2, 2) 和所有陆地区域之间的距离都达到最大，最大距离为 4。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= grid.length == grid[0].length <= 100
+ * grid[i][j] 不是 0 就是 1
+ * 通过次数6,664提交次数17,316
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/as-far-from-land-as-possible
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxDistanceMM {
+
+    public int maxDistance(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+        if (queue.isEmpty() || queue.size() == m * n) {
+            return -1;
+        }
+        int[][] next = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+        int level = -1;
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                int[] cur = queue.poll();
+                if (cur == null) {
+                    continue;
+                }
+                for (int[] nt : next) {
+                    int nx = cur[0] - nt[0], ny = cur[1] - nt[1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 0) {
+                        grid[nx][ny] = 1;
+                        queue.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+}
