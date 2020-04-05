b5e65c552b7b689d0fb2cd066f28b64b33c869ec
DusongWang <dasong.wds@alibaba-inc.com>
2020-04-05 19:49:01 +0800

    find-lucky-integer-in-an-array

---
+++ src/com/leetcode/offer/code/FindLucky.java
@@ -0,0 +1,65 @@
+package com.leetcode.offer.code;
+
+/**
+ * @author duson
+ * 在整数数组中，如果一个整数的出现频次和它的数值大小相等，我们就称这个整数为「幸运数」。
+ * <p>
+ * 给你一个整数数组 arr，请你从中找出并返回一个幸运数。
+ * <p>
+ * 如果数组中存在多个幸运数，只需返回 最大 的那个。
+ * 如果数组中不含幸运数，则返回 -1 。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [2,2,3,4]
+ * 输出：2
+ * 解释：数组中唯一的幸运数是 2 ，因为数值 2 的出现频次也是 2 。
+ * 示例 2：
+ * <p>
+ * 输入：arr = [1,2,2,3,3,3]
+ * 输出：3
+ * 解释：1、2 以及 3 都是幸运数，只需要返回其中最大的 3 。
+ * 示例 3：
+ * <p>
+ * 输入：arr = [2,2,2,3,3]
+ * 输出：-1
+ * 解释：数组中不存在幸运数。
+ * 示例 4：
+ * <p>
+ * 输入：arr = [5]
+ * 输出：-1
+ * 示例 5：
+ * <p>
+ * 输入：arr = [7,7,7,7,7,7,7]
+ * 输出：7
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= arr.length <= 500
+ * 1 <= arr[i] <= 500
+ * 通过次数4,758提交次数6,972
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-lucky-integer-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindLucky {
+
+    public int findLucky(int[] arr) {
+        int len = arr.length;
+        int[] cnt = new int[501];
+        for (int a : arr) {
+            cnt[a]++;
+        }
+        int max = -1;
+        for (int a : arr) {
+            if (a == cnt[a]) {
+                max = Math.max(max, a);
+            }
+        }
+        return max;
+    }
+}
