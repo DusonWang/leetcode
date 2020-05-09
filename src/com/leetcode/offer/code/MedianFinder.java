5b0a5ab53347ecb80c9f9ccb1f49157efc5ef265
DusongWang <dasong.wds@alibaba-inc.com>
2020-05-09 22:13:30 +0800

    find-median-from-data-stream 2

--- src/com/leetcode/offer/code/MedianFinder.java
+++ src/com/leetcode/offer/code/MedianFinder.java
@@ -73,16 +73,4 @@ public class MedianFinder {
             return (double) maxHeap.peek();
         }
     }
-
-    public static void main(String[] args) {
-        MedianFinder obj = new MedianFinder();
-        obj.addNum(1);
-        obj.addNum(2);
-
-        System.out.println(obj.findMedian());
-
-        obj.addNum(3);
-        System.out.println(obj.findMedian());
-
-    }
 }
