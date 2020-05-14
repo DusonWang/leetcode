a133dfc29e87f7ca52857b0cba0bf2b327f42add
DusongWang <dasong.wds@alibaba-inc.com>
2020-05-14 21:58:03 +0800

    longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit

---
+++ src/com/leetcode/offer/code/LongestSubarray.java
@@ -0,0 +1,75 @@
+package com.leetcode.offer.code;
+
+/**
+ * 给你一个整数数组 nums ，和一个表示限制的整数 limit，请你返回最长连续子数组的长度，该子数组中的任意两个元素之间的绝对差必须小于或者等于 limit 。
+ * <p>
+ * 如果不存在满足条件的子数组，则返回 0 。
+ * <p>
+ * ?
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [8,2,4,7], limit = 4
+ * 输出：2
+ * 解释：所有子数组如下：
+ * [8] 最大绝对差 |8-8| = 0 <= 4.
+ * [8,2] 最大绝对差 |8-2| = 6 > 4.
+ * [8,2,4] 最大绝对差 |8-2| = 6 > 4.
+ * [8,2,4,7] 最大绝对差 |8-2| = 6 > 4.
+ * [2] 最大绝对差 |2-2| = 0 <= 4.
+ * [2,4] 最大绝对差 |2-4| = 2 <= 4.
+ * [2,4,7] 最大绝对差 |2-7| = 5 > 4.
+ * [4] 最大绝对差 |4-4| = 0 <= 4.
+ * [4,7] 最大绝对差 |4-7| = 3 <= 4.
+ * [7] 最大绝对差 |7-7| = 0 <= 4.
+ * 因此，满足题意的最长子数组的长度为 2 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [10,1,2,4,7,2], limit = 5
+ * 输出：4
+ * 解释：满足题意的最长子数组是 [2,4,7,2]，其最大绝对差 |2-7| = 5 <= 5 。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [4,2,2,2,4,4,2,2], limit = 0
+ * 输出：3
+ * ?
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^9
+ * 0 <= limit <= 10^9
+ * 通过次数3,446提交次数9,179
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestSubarray {
+
+    public int longestSubarray(int[] nums, int limit) {
+        int n = nums.length;
+        int maxI = 0;
+        int minI = 0;
+        int ans = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            if (nums[r] > nums[maxI]) {
+                maxI = r;
+            }
+            if (nums[r] < nums[minI]) {
+                minI = r;
+            }
+            while (nums[maxI] - nums[minI] > limit) {
+                if (maxI < minI) {
+                    l = ++maxI;
+                } else {
+                    l = ++minI;
+                }
+                r = l;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}
