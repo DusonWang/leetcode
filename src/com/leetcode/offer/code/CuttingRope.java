8faabbc86cd60363382789b4e1b56faba2c992bb
DusongWang <dasong.wds@alibaba-inc.com>
2020-05-20 17:56:29 +0800

    jian-sheng-zi-lcof 1 2

---
+++ src/com/leetcode/offer/code/CuttingRope.java
@@ -0,0 +1,56 @@
+package com.leetcode.offer.code;
+
+/**
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * 示例?2:
+ * <p>
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 ×?3 ×?4 = 36
+ * 提示：
+ * <p>
+ * 2 <= n <= 58
+ * 注意：本题与主站 343 题相同：https://leetcode-cn.com/problems/integer-break/
+ * <p>
+ * 通过次数19,261提交次数35,543
+ * 在真实的面试中遇到过这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CuttingRope {
+
+    public int cuttingRope(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
+    public int cuttingRope2(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        long res = 1;
+        while (n > 4) {
+            res *= 3;
+            res %= 1000000007;
+            n -= 3;
+        }
+        return (int) (res * n % 1000000007);
+    }
+}
