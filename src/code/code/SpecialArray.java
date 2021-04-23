48855d859890e1adb855b8a9a2d85be8963bfdd4
wangdasong <wangdasong@bilibili.com>
2021-04-23 20:48:11 +0800

    add

--- src/code/code/SpecialArray.java
+++ src/code/code/SpecialArray.java
@@ -62,4 +62,18 @@ public class SpecialArray {
         return res;
     }
 
+    public int specialArray2(int[] nums) {
+        int n = nums.length, cnt = 0;
+        int[] counter = new int[n + 1];
+        for (int num : nums) {
+            counter[Math.min(num, n)]++;
+        }
+        for (int i = n; i > 0; i--) {
+            cnt += counter[i];
+            if (cnt == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
