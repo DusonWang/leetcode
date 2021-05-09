9179479f050a499bbc50dfe2d808880b5508faf9
wangdasong <wangdasong@bilibili.com>
2021-05-09 18:04:48 +0800

    count-sorted-vowel-strings

--- src/code/code/BreakfastNumber.java
+++ src/code/code/BreakfastNumber.java
@@ -147,4 +147,54 @@ public class BreakfastNumber {
         }
         return ans % mod;
     }
+
+    /**
+     * 给你一个整数 n，请返回长度为 n 、仅由元音 (a, e, i, o, u) 组成且按 字典序排列 的字符串数量。
+     * <p>
+     * 字符串 s 按 字典序排列 需要满足：对于所有有效的 i，s[i] 在字母表中的位置总是与 s[i+1] 相同或在 s[i+1] 之前。
+     * <p>
+     * ?
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：n = 1
+     * 输出：5
+     * 解释：仅由元音组成的 5 个字典序字符串为 ["a","e","i","o","u"]
+     * 示例 2：
+     * <p>
+     * 输入：n = 2
+     * 输出：15
+     * 解释：仅由元音组成的 15 个字典序字符串为
+     * ["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"]
+     * 注意，"ea" 不是符合题意的字符串，因为 'e' 在字母表中的位置比 'a' 靠后
+     * 示例 3：
+     * <p>
+     * 输入：n = 33
+     * 输出：66045
+     * ?
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= n <= 50?
+     * 通过次数10,478提交次数13,624
+     * 请问您在哪类招聘中遇到此题？
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/count-sorted-vowel-strings
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int countVowelStrings(int n) {
+        int[] dp = new int[5];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < 5; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        for (int cnt : dp) {
+            res += cnt;
+        }
+        return res;
+    }
 }
