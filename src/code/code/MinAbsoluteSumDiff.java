31f20e4030759311976e7132ce5c1e9dff8e6926
wangdasong <wangdasong@bilibili.com>
2021-05-21 17:14:36 +0800

    minimum-absolute-sum-difference

---
+++ src/code/code/MinAbsoluteSumDiff.java
@@ -0,0 +1,81 @@
+package code.code;
+
+import java.util.TreeSet;
+
+/**
+ * 给你两个正整数数组 nums1 和 nums2 ，数组的长度都是 n 。
+ * <p>
+ * 数组 nums1 和 nums2 的 绝对差值和 定义为所有 |nums1[i] - nums2[i]|（0 <= i < n）的 总和（下标从 0 开始）。
+ * <p>
+ * 你可以选用 nums1 中的 任意一个 元素来替换 nums1 中的 至多 一个元素，以 最小化 绝对差值和。
+ * <p>
+ * 在替换数组 nums1 中最多一个元素 之后 ，返回最小绝对差值和。因为答案可能很大，所以需要对 109 + 7 取余 后返回。
+ * <p>
+ * |x| 定义为：
+ * <p>
+ * 如果 x >= 0 ，值为 x ，或者
+ * 如果 x <= 0 ，值为 -x
+ * ?
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,7,5], nums2 = [2,3,5]
+ * 输出：3
+ * 解释：有两种可能的最优方案：
+ * - 将第二个元素替换为第一个元素：[1,7,5] => [1,1,5] ，或者
+ * - 将第二个元素替换为第三个元素：[1,7,5] => [1,5,5]
+ * 两种方案的绝对差值和都是 |1-2| + (|1-3| 或者 |5-3|) + |5-5| = 3
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [2,4,6,8,10], nums2 = [2,4,6,8,10]
+ * 输出：0
+ * 解释：nums1 和 nums2 相等，所以不用替换元素。绝对差值和为 0
+ * 示例 3：
+ * <p>
+ * 输入：nums1 = [1,10,4,4,2,7], nums2 = [9,3,5,1,7,4]
+ * 输出：20
+ * 解释：将第一个元素替换为第二个元素：[1,10,4,4,2,7] => [10,10,4,4,2,7]
+ * 绝对差值和为 |10-9| + |10-3| + |4-5| + |4-1| + |2-7| + |7-4| = 20
+ * ?
+ * <p>
+ * 提示：
+ * <p>
+ * n == nums1.length
+ * n == nums2.length
+ * 1 <= n <= 105
+ * 1 <= nums1[i], nums2[i] <= 105
+ * 通过次数5,383提交次数13,729
+ * 请问您在哪类招聘中遇到此题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-absolute-sum-difference
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinAbsoluteSumDiff {
+
+    public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
+        TreeSet<Integer> set = new TreeSet<>();
+        int sum = 0;
+        int diff = 0;
+        int m = (int) 1e9 + 7;
+        for (int i = 0; i < nums1.length; i++) {
+            sum = (sum + Math.abs(nums1[i] - nums2[i])) % m;
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            int tmp = Math.abs(nums1[i] - nums2[i]);
+            if (set.floor(nums2[i]) != null) {
+                diff = Math.max(diff, tmp - Math.abs(set.floor(nums2[i]) - nums2[i]));
+            }
+            if (set.ceiling(nums2[i]) != null) {
+                diff = Math.max(diff, tmp - Math.abs(set.ceiling(nums2[i]) - nums2[i]));
+            }
+        }
+        return (sum - diff) % m;
+    }
+
+    public static void main(String[] args) {
+        MinAbsoluteSumDiff minAbsoluteSumDiff = new MinAbsoluteSumDiff();
+        System.out.println(minAbsoluteSumDiff.minAbsoluteSumDiff(new int[]{1, 28, 21}, new int[]{9, 21, 20}));
+    }
+}
