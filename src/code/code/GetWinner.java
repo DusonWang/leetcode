278e47d1deff334117a67c8db24f4e0c756603ac
wangdasong <wangdasong@bilibili.com>
2021-05-27 11:07:56 +0800

    find-the-winner-of-an-array-game

---
+++ src/code/code/GetWinner.java
@@ -0,0 +1,74 @@
+package code.code;
+
+/**
+ * 给你一个由 不同 整数组成的整数数组 arr 和一个整数 k 。
+ * <p>
+ * 每回合游戏都在数组的前两个元素（即 arr[0] 和 arr[1] ）之间进行。比较 arr[0] 与 arr[1] 的大小，较大的整数将会取得这一回合的胜利并保留在位置 0 ，较小的整数移至数组的末尾。当一个整数赢得 k 个连续回合时，游戏结束，该整数就是比赛的 赢家 。
+ * <p>
+ * 返回赢得比赛的整数。
+ * <p>
+ * 题目数据 保证 游戏存在赢家。
+ * <p>
+ * ?
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [2,1,3,5,4,6,7], k = 2
+ * 输出：5
+ * 解释：一起看一下本场游戏每回合的情况：
+ * <p>
+ * 因此将进行 4 回合比赛，其中 5 是赢家，因为它连胜 2 回合。
+ * 示例 2：
+ * <p>
+ * 输入：arr = [3,2,1], k = 10
+ * 输出：3
+ * 解释：3 将会在前 10 个回合中连续获胜。
+ * 示例 3：
+ * <p>
+ * 输入：arr = [1,9,8,2,3,7,6,4,5], k = 7
+ * 输出：9
+ * 示例 4：
+ * <p>
+ * 输入：arr = [1,11,22,33,44,55,66,77,88,99], k = 1000000000
+ * 输出：99
+ * ?
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= arr.length <= 10^5
+ * 1 <= arr[i] <= 10^6
+ * arr 所含的整数 各不相同 。
+ * 1 <= k <= 10^9
+ * 通过次数12,579提交次数27,878
+ * 请问您在哪类招聘中遇到此题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-winner-of-an-array-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GetWinner {
+
+    public int getWinner(int[] arr, int k) {
+        int prev = Math.max(arr[0], arr[1]);
+        if (k == 1) {
+            return prev;
+        }
+        int consecutive = 1;
+        int maxNum = prev;
+        int length = arr.length;
+        for (int i = 2; i < length; i++) {
+            int curr = arr[i];
+            if (prev > curr) {
+                consecutive++;
+                if (consecutive == k) {
+                    return prev;
+                }
+            } else {
+                prev = curr;
+                consecutive = 1;
+            }
+            maxNum = Math.max(maxNum, curr);
+        }
+        return maxNum;
+    }
+}
