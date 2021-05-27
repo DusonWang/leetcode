05a8f2858071860d0309983a48328d65ad11fc54
wangdasong <wangdasong@bilibili.com>
2021-05-27 14:48:02 +0800

    luogu a+b

---
+++ src/code/luogu/MainAB.java
@@ -0,0 +1,12 @@
+package code.luogu;
+
+import java.io.*;
+import java.util.*;
+
+public class MainAB {
+    public static void main(String[] args) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt(), b = cin.nextInt();
+        System.out.println(a + b);
+    }
+}
