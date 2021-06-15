d93cd86688c29fce8ee42229e34db25b521b8d0d
wangdasong <wangdasong@bilibili.com>
2021-06-15 11:20:06 +0800

    fix peak-index-in-a-mountain-array

--- src/code/code/PeakIndexInMountainArray.java
+++ src/code/code/PeakIndexInMountainArray.java
@@ -37,9 +37,9 @@ public class PeakIndexInMountainArray {
         int j = a.length - 1;
         while (i <= j) {
             int mid = i + ((j - i) >> 1);
-            if (a[mid] > a[mid - 1] && a[mid] > a[mid + 1]) {
+            if (mid >= 1 && a[mid] > a[mid - 1] && a[mid] > a[mid + 1]) {
                 return mid;
-            } else if (a[mid] < a[mid - 1] && a[mid] > a[mid + 1]) {
+            } else if (mid >= 1 && a[mid] < a[mid - 1] && a[mid] > a[mid + 1]) {
                 j = mid - 1;
             } else {
                 i = mid + 1;
@@ -48,4 +48,8 @@ public class PeakIndexInMountainArray {
         return -1;
     }
 
+    public static void main(String[] args) {
+        PeakIndexInMountainArray peakIndexInMountainArray = new PeakIndexInMountainArray();
+        System.out.println(peakIndexInMountainArray.peakIndexInMountainArray(new int[]{3, 5, 3, 2, 0}));
+    }
 }
