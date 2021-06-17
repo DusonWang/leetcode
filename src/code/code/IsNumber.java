1ace961a4b2df368b320fa7142301104d1eb3c77
wangdasong <wangdasong@bilibili.com>
2021-06-17 11:09:21 +0800

    valid-number

--- src/code/code/IsNumber.java
+++ src/code/code/IsNumber.java
@@ -7,22 +7,22 @@ package code.code;
  * <p>
  * 例如:
  * <p>
- * "0" => true
- * " 0.1 " => true
- * "abc" => false
- * "1 a" => false
- * "2e10" => true
- * " -90e3   " => true
- * " 1e" => false
- * "e3" => false
- * " 6e-1" => true
- * " 99e2.5 " => false
- * "53.5e93" => true
- * " --6 " => false
- * "-+3" => false
- * "95a54e53" => false
+ * "0"=>true
+ * " 0.1 "=>true
+ * "abc"=>false
+ * "1 a"=>false
+ * "2e10"=>true
+ * " -90e3 "=>true
+ * " 1e"=>false
+ * "e3"=>false
+ * " 6e-1"=>true
+ * " 99e2.5"=>false
+ * "53.5e93"=>true
+ * " --6 "=>false
+ * "-+3"=>false
+ * "95a54e53"=>false
  * <p>
- * 说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。这里给出一份可能存在于有效十进制数字中的字符列表：
+ * 说明:我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。这里给出一份可能存在于有效十进制数字中的字符列表：
  * <p>
  * 数字 0-9
  * 指数 - "e"
@@ -31,7 +31,7 @@ package code.code;
  * 当然，在输入中，这些字符的上下文也很重要。
  * <p>
  * 更新于 2015-02-10:
- * C++函数的形式已经更新了。如果你仍然看见你的函数接收 const char * 类型的参数，请点击重载按钮重置你的代码。
+ * C++函数的形式已经更新了。如果你仍然看见你的函数接收const char * 类型的参数，请点击重载按钮重置你的代码。
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/valid-number
@@ -40,42 +40,41 @@ package code.code;
 public class IsNumber {
 
     public boolean isNumber(String s) {
+        if (s == null || s.trim().length() == 0) {
+            return false;
+        }
+        boolean numSeen = false;
+        boolean dotSeen = false;
+        boolean eSeen = false;
+        boolean numberAfterE = false;
 
         s = s.trim();
-
-        boolean pointSeen = false;
-
-        boolean eSeen = false;
-
-        boolean numberSeen = false;
-
-        boolean numberAfterE = true;
-
         for (int i = 0; i < s.length(); i++) {
-            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
-                numberSeen = true;
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
                 numberAfterE = true;
-            } else if (s.charAt(i) == '.') {
-                if (eSeen || pointSeen) {
-                    return false;
-                }
-                pointSeen = true;
-            } else if (s.charAt(i) == 'e') {
-                if (eSeen || !numberSeen) {
+                numSeen = true;
+            } else if (c == 'e' || c == 'E') {
+                if (!numSeen || eSeen) {
                     return false;
                 }
                 numberAfterE = false;
                 eSeen = true;
-            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
-                if (i != 0 && s.charAt(i - 1) != 'e') {
+            } else if (c == '.') {
+                if (dotSeen || eSeen) {
+                    return false;
+                }
+                dotSeen = true;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e') {
                     return false;
                 }
             } else {
                 return false;
             }
-        }
 
-        return numberSeen && numberAfterE;
+        }
+        return numSeen && numberAfterE;
     }
 
     public static void main(String[] args) {
