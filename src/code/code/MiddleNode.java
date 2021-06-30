620ad1aa2133179890be05a87ac42282f088faa3
wangdasong <wangdasong@bilibili.com>
2021-06-30 14:33:39 +0800

    middle-of-the-linked-list 2

--- src/code/code/MiddleNode.java
+++ src/code/code/MiddleNode.java
@@ -44,7 +44,8 @@ public class MiddleNode {
     }
 
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head, fast = head;
+        ListNode slow = head;
+        ListNode fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
